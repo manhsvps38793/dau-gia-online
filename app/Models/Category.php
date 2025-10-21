4bd2a6ef475004618f872be98c42893a6e67e946
nam <nambpps38619@gmail.com>
2025-10-21 11:05:54 +0700

    fix categoriees

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 
 class Category extends BaseModel
 {
+    protected $primaryKey = 'category_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+    protected $fillable = ['name', 'description'];
 
+    // Định nghĩa mối quan hệ với AuctionItem   
+    public function auctionItems()
+    {
+        return $this->hasMany(AuctionItem::class, 'category_id', 'category_id');
+    }
 }
