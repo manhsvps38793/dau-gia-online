b097f1af8561c9a7d715b79ce22956bcfd17174a
Sầm Văn Mạnh <manhsvps38793@gmail.com>
2025-10-22 08:53:20 +0700

    Lưu thay đổi trước khi lấy code mới

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-// use App\Events\TestEvent;
+use Illuminate\Support\Facades\Route;
+use App\Events\TestEvent;
 
-// Route::get('/test-broadcast', function () {
-//     broadcast(new TestEvent('Xin chào từ Laravel!'));
-//     return 'Đã gửi event tới Node.js!';
-// });
+Route::get('/test-broadcast', function () {
+    broadcast(new TestEvent('Xin chào từ Laravel!'));
+    return 'Đã gửi event tới Node.js!';
+});
 
